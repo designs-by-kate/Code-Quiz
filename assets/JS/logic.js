fddc27523ab3b2fb3851735f6ceaa4be618003db
Pawel Pospiech <kate_diet@hotmail.com>
2024-01-14 21:06:49 +0000

    Improve code readability through tidying and reorganization

--- assets/JS/logic.js
+++ assets/JS/logic.js
@@ -1,3 +1,4 @@
+//Initialize variables
 var score = 0;
 var timeLeft = 60;
 var currentQuestionIndex = 0;
@@ -17,42 +18,32 @@ function showDiv(divId) {
   element.classList.add("start"); // Add "start" class to display the element
 }
 
-//press button "StartQuiz" to start
+// Function to play sound
+function playSound(soundId) {
+  var sound = document.getElementById(soundId);
+  sound.play();
+}
 
-// Event listener for the "Start Quiz" button
-var startBtn = document.getElementById("start");
-startBtn.addEventListener("click", function () {
-//question DIV open, and start DIV hide
-    hideDiv("start-screen"); // Hide the start screen
-    showDiv("questions"); // Show the questions div
-    countdown();
-});
-
-//display questions
+//Function to display questions
 function displayQuestion(){
   var questionTitleElement = document.getElementById("question-title");
   var choicesContainer = document.getElementById("choices");
   var options = questions[currentQuestionIndex].options;
 
-  //if (currentQuestionIndex < questions.length){
+  //Display question and answer options
     questionTitleElement.textContent = questions[currentQuestionIndex].questionTitle;
-    // Clear previous choices
     choicesContainer.innerHTML = "";
 
-      // Display answer options
     for (var option = 0; option < options.length; option++) {
       var optionBtn = document.createElement("button");
       optionBtn.textContent = options[option];
       choicesContainer.appendChild(optionBtn);
 
+      //Event listener for user's answer click
       optionBtn.addEventListener("click", function (event) {
         handleAnswerClick(event.target.textContent);
       });
     }
-  // }else{
-  //   // No more questions, end the quiz
-  //     endQuiz();
-  // }
 }
 
 // Function to handle user's answer click
@@ -60,19 +51,19 @@ function handleAnswerClick(selectedAnswer) {
   var result = document.querySelector("#result")
 
   if (selectedAnswer === questions[currentQuestionIndex].correctAnswer) {
-      score += 10;
-      playSound("correctSound");
-      result.textContent = "Correct!";
-      // Display sound for correct answer
+      score += 10; //Increase score for correct answer
+      playSound("correctSound"); // Display sound for correct answer
+      result.textContent = "Correct!"; 
   } else if (selectedAnswer != questions[currentQuestionIndex].correctAnswer && timeLeft < 10){
+    // If incorrect answer and less than 10 seconds left, end the quiz, and set timer to 0;
     timeLeft = 0;
     endQuiz();
   }
   else {
+    //Decrease time for incorrect answer
     timeLeft -= 10;
-    playSound("incorrectSound");
+    playSound("incorrectSound"); // Display sound for incorrect answer
     result.textContent = "Wrong!";
-    // Display sound for incorrect answer
   }
 
   // Move to the next question
@@ -83,20 +74,15 @@ function handleAnswerClick(selectedAnswer) {
     displayQuestion();
   } else {
     // All questions are done
-    console.log("Quiz completed! Score: " + score);
-    // Add code for quiz completion
+    result.textContent = "Quiz completed! Score: " + score;
+    endQuiz();
   }
 }
 
-// Function to play sound
-function playSound(soundId) {
-  var sound = document.getElementById(soundId);
-  sound.play();
-}
-
+//Function for countdown timer
 function countdown() {
   var timer = document.querySelector("#time");
-  //setInterval(function, delay). function: The function to be executed at each interval. delay: The time, in milliseconds, between each execution of the function.
+  // Update the timer element with the remaining time every second
   var timeInterval = setInterval(function () {
     // Check if there is time left in the countdown, and no more questions available
     if (currentQuestionIndex < questions.length && timeLeft >= 0){
@@ -105,7 +91,7 @@ function countdown() {
     // Decrease the remaining time by 1 second
     timeLeft--;
     }else{
-    // If the countdown reaches zero, clear the interval to stop the countdown
+    // If countdown reaches zero or no more questions, end the quiz
       clearInterval(timeInterval);
       // Call the function to display the message
       endQuiz();
@@ -113,28 +99,28 @@ function countdown() {
   }, 1000); // Set the interval to 1000 milliseconds (1 second)
 }
 
-// Initial call to display the first question
-displayQuestion();
-
-////after quiz finish
-
-//question DIV hide and end DIV open
+// Function to end the quiz and display the final score
 function endQuiz(){
   hideDiv("questions"); // Hide the questions div
   showDiv("end-screen"); // Show the end screen
   var finalScoreElement = document.getElementById("final-score");
   finalScoreElement.textContent = score;
-   // Include your logic for displaying the final score and handling initials submission
-  // var finalScoreElement = document.getElementById("final-score");
-  // finalScoreElement.textContent = score;
 }
 
+//Function to restart the quiz
 function reStartQuiz(){
   hideDiv("end-screen"); // Hide the questions div
   showDiv("start-screen"); // Show the end screen
 }
-//press SUBMIT button, to end game and move back to the start to play again
-//end DIV hide start DIV open
+
+// Event listener for the "Start Quiz" button
+var startBtn = document.getElementById("start");
+startBtn.addEventListener("click", function () {
+// Hide the start screen and show the questions div
+    hideDiv("start-screen");
+    showDiv("questions");
+    countdown(); //when game starts, timer sets
+});
 
 // Event listener for the "Submit" button
 var submitBtn = document.getElementById("submit");
@@ -157,3 +143,6 @@ submitBtn.addEventListener("click", function () {
   }
   reStartQuiz();
 });
+
+// Initial call to display the first question
+displayQuestion();
